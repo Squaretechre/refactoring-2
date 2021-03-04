09064980c3adb0e42613de9fd55e1150bbc3c93d
Dan Parkin <danielparkin1@gmail.com>
2021-03-04 09:02:34 +0000

    feat: add transfer function to bank account exercise

--- src/bank-account/bank-account-service.ts
+++ src/bank-account/bank-account-service.ts
@@ -5,10 +5,35 @@ export interface Transaction {
   date: Date
 }
 
-interface Result {
+interface Result<T> {
   isSuccess: boolean
-  transactions?: Transaction[]
+  value?: T
   reason?: string
+}
+
+interface Transfer {
+  originTransactions: Transaction[]
+  destinationTransactions: Transaction[]
+}
+
+export const balance = (
+  accountNumber: number,
+  sortCode: number,
+  transactions: Transaction[],
+  isClosed: boolean,
+  isFrozen: boolean
+): Result<number> => {
+  if (isClosed) return { isSuccess: false, reason: 'Account is closed.' }
+  if (isFrozen) return { isSuccess: false, reason: 'Account is currently frozen.' }
+
+  const balance = transactions
+    .sort((a, b) => b.date.getTime() - a.date.getTime())
+    .reduce((balance: number, transaction) => balance + transaction.amount, 0)
+
+  return {
+    isSuccess: true,
+    value: balance,
+  }
 }
 
 export const withdrawFromAccount = (
@@ -19,20 +44,18 @@ export const withdrawFromAccount = (
   isClosed: boolean,
   isFrozen: boolean,
   now: Date
-): Result => {
+): Result<Transaction[]> => {
   if (isClosed) return { isSuccess: false, reason: 'Account is closed.' }
   if (isFrozen) return { isSuccess: false, reason: 'Account is currently frozen.' }
 
-  const balance = transactions
-    .sort((a, b) => b.date.getTime() - a.date.getTime())
-    .reduce((balance: number, transaction) => balance + transaction.amount, 0)
+  const accountBalance = balance(accountNumber, sortCode, transactions, isClosed, isFrozen).value
 
-  if (balance < amountToWithdraw)
+  if (accountBalance < amountToWithdraw)
     return { isSuccess: false, reason: 'Account has insufficient funds.' }
 
   return {
     isSuccess: true,
-    transactions: [
+    value: [
       {
         accountNumber,
         sortCode,
@@ -52,7 +75,7 @@ export const depositIntoAccount = (
   isClosed: boolean,
   isFrozen: boolean,
   now: Date
-): Result => {
+): Result<Transaction[]> => {
   if (isClosed) return { isSuccess: false, reason: 'Account is closed.' }
   if (isFrozen) return { isSuccess: false, reason: 'Account is currently frozen.' }
   if (amountToDeposit === 0) return { isSuccess: false, reason: 'Can not deposit 0 funds.' }
@@ -60,7 +83,7 @@ export const depositIntoAccount = (
 
   return {
     isSuccess: true,
-    transactions: [
+    value: [
       {
         accountNumber,
         sortCode,
@@ -69,5 +92,68 @@ export const depositIntoAccount = (
       },
       ...transactions,
     ],
+  }
+}
+
+export const transferFunds = (
+  amountToTransfer: number,
+  originAccountNumber: number,
+  originSortCode: number,
+  originTransactions: Transaction[],
+  isOriginClosed: boolean,
+  isOriginFrozen: boolean,
+  destinationAccountNumber: number,
+  destinationSortCode: number,
+  destinationTransactions: Transaction[],
+  isDestinationClosed: boolean,
+  isDestinationFrozen: boolean,
+  now: Date
+): Result<Transfer> => {
+  if (isOriginClosed) return { isSuccess: false, reason: 'Origin account is closed.' }
+  if (isDestinationClosed) return { isSuccess: false, reason: 'Destination account is closed.' }
+  if (isOriginFrozen) return { isSuccess: false, reason: 'Origin account is currently frozen.' }
+  if (isDestinationFrozen)
+    return { isSuccess: false, reason: 'Destination account is currently frozen.' }
+
+  const originAccountBalance = balance(
+    originAccountNumber,
+    originSortCode,
+    originTransactions,
+    isOriginClosed,
+    isOriginFrozen
+  ).value
+
+  if (originAccountBalance < amountToTransfer)
+    return {
+      isSuccess: false,
+      reason: 'Origin account has insufficient funds to complete transfer.',
+    }
+
+  const originTransactionsAfterTransfer = [
+    {
+      accountNumber: originAccountNumber,
+      sortCode: originSortCode,
+      amount: -amountToTransfer,
+      date: now,
+    },
+    ...originTransactions,
+  ]
+
+  const destinationTransactionsAfterTransfer = [
+    {
+      accountNumber: destinationAccountNumber,
+      sortCode: destinationSortCode,
+      amount: amountToTransfer,
+      date: now,
+    },
+    ...destinationTransactions,
+  ]
+
+  return {
+    isSuccess: true,
+    value: {
+      originTransactions: originTransactionsAfterTransfer,
+      destinationTransactions: destinationTransactionsAfterTransfer,
+    },
   }
 }
--- test/bank-account/bank-account-service.test.ts
+++ test/bank-account/bank-account-service.test.ts
@@ -1,10 +1,57 @@
 import {
+  balance,
   depositIntoAccount,
   Transaction,
+  transferFunds,
   withdrawFromAccount,
 } from '../../src/bank-account/bank-account-service'
 
 describe('bank account service', () => {
+  describe('getting the current account balance', () => {
+    it('gets the current balance of the account', () => {
+      const now: Date = new Date()
+      const yesterday: Date = new Date(now)
+      const dayBeforeYesterday: Date = new Date(now)
+
+      yesterday.setDate(now.getDate() - 1)
+      dayBeforeYesterday.setDate(now.getDate() - 2)
+
+      const currentTransactions: Transaction[] = [
+        {
+          accountNumber: 10203040,
+          sortCode: 102030,
+          amount: 150,
+          date: yesterday,
+        },
+        {
+          accountNumber: 10203040,
+          sortCode: 102030,
+          amount: 50,
+          date: dayBeforeYesterday,
+        },
+      ]
+
+      const result = balance(10203040, 102030, currentTransactions, false, false)
+
+      expect(result.isSuccess).toBeTruthy()
+      expect(result.value).toBe(200)
+    })
+
+    it('does not get the current balance of the account when the account is closed', () => {
+      const result = balance(10203040, 102030, [], true, false)
+
+      expect(result.isSuccess).toBeFalsy()
+      expect(result.reason).toBe('Account is closed.')
+    })
+
+    it('does not get the current balance of the account when the account is frozen', () => {
+      const result = balance(10203040, 102030, [], false, true)
+
+      expect(result.isSuccess).toBeFalsy()
+      expect(result.reason).toBe('Account is currently frozen.')
+    })
+  })
+
   describe('withdrawing funds', () => {
     it('withdraws funds when there are enough funds in the account to cover the withdrawal', () => {
       const now: Date = new Date()
@@ -39,11 +86,15 @@ describe('bank account service', () => {
         now
       )
 
+      let mostRecentTransaction = result.value[0]
+      const balanceAfterWithdrawal = balance(10203040, 102030, result.value, false, false)
+
       expect(result.isSuccess).toBeTruthy()
-      expect(result.transactions[0].accountNumber).toEqual(10203040)
-      expect(result.transactions[0].sortCode).toEqual(102030)
-      expect(result.transactions[0].amount).toEqual(-100)
-      expect(result.transactions[0].date).toEqual(now)
+      expect(balanceAfterWithdrawal.value).toBe(100)
+      expect(mostRecentTransaction.accountNumber).toBe(10203040)
+      expect(mostRecentTransaction.sortCode).toBe(102030)
+      expect(mostRecentTransaction.amount).toBe(-100)
+      expect(mostRecentTransaction.date).toBe(now)
     })
 
     it('does not withdraw funds when there are not enough funds in the account to cover the withdrawal', () => {
@@ -132,11 +183,15 @@ describe('bank account service', () => {
         now
       )
 
+      let mostRecentTransaction = result.value[0]
+      const balanceAfterWithdrawal = balance(10203040, 102030, result.value, false, false)
+
       expect(result.isSuccess).toBeTruthy()
-      expect(result.transactions[0].accountNumber).toEqual(10203040)
-      expect(result.transactions[0].sortCode).toEqual(102030)
-      expect(result.transactions[0].amount).toEqual(666)
-      expect(result.transactions[0].date).toEqual(now)
+      expect(balanceAfterWithdrawal.value).toBe(866)
+      expect(mostRecentTransaction.accountNumber).toBe(10203040)
+      expect(mostRecentTransaction.sortCode).toBe(102030)
+      expect(mostRecentTransaction.amount).toBe(666)
+      expect(mostRecentTransaction.date).toBe(now)
     })
 
     it('does not allow 0 funds to be deposited', () => {
@@ -165,6 +220,195 @@ describe('bank account service', () => {
 
       expect(result.isSuccess).toBeFalsy()
       expect(result.reason).toBe('Account is currently frozen.')
+    })
+  })
+
+  describe('transferring funds', () => {
+    it('transfers funds from the origin account to the destination account when there are enough funds in the account to cover the transfer', () => {
+      const now: Date = new Date()
+      const yesterday: Date = new Date(now)
+      const dayBeforeYesterday: Date = new Date(now)
+
+      yesterday.setDate(now.getDate() - 1)
+      dayBeforeYesterday.setDate(now.getDate() - 2)
+
+      const currentOriginTransactions: Transaction[] = [
+        {
+          accountNumber: 10203040,
+          sortCode: 102030,
+          amount: 150,
+          date: yesterday,
+        },
+        {
+          accountNumber: 10203040,
+          sortCode: 102030,
+          amount: 50,
+          date: dayBeforeYesterday,
+        },
+      ]
+
+      const result = transferFunds(
+        30,
+        10203040,
+        102030,
+        currentOriginTransactions,
+        false,
+        false,
+        50607080,
+        405060,
+        [],
+        false,
+        false,
+        now
+      )
+
+      let mostRecentOriginTransaction = result.value.originTransactions[0]
+      let mostRecentDestinationTransaction = result.value.destinationTransactions[0]
+
+      const originBalanceAfterTransfer = balance(
+        10203040,
+        102030,
+        result.value.originTransactions,
+        false,
+        false
+      )
+
+      const destinationBalanceAfterTransfer = balance(
+        50607080,
+        405060,
+        result.value.destinationTransactions,
+        false,
+        false
+      )
+
+      expect(result.isSuccess).toBeTruthy()
+
+      expect(originBalanceAfterTransfer.value).toBe(170)
+      expect(destinationBalanceAfterTransfer.value).toBe(30)
+
+      expect(mostRecentOriginTransaction.accountNumber).toBe(10203040)
+      expect(mostRecentOriginTransaction.sortCode).toBe(102030)
+      expect(mostRecentOriginTransaction.amount).toBe(-30)
+      expect(mostRecentOriginTransaction.date).toBe(now)
+
+      expect(mostRecentDestinationTransaction.accountNumber).toBe(50607080)
+      expect(mostRecentDestinationTransaction.sortCode).toBe(405060)
+      expect(mostRecentDestinationTransaction.amount).toBe(30)
+      expect(mostRecentDestinationTransaction.date).toBe(now)
+    })
+
+    it('does not transfer funds when there are not enough funds in the origin account to cover the transfer', () => {
+      const now: Date = new Date()
+      const yesterday: Date = new Date(now)
+
+      yesterday.setDate(now.getDate() - 1)
+
+      const currentOriginTransactions: Transaction[] = [
+        {
+          accountNumber: 10203040,
+          sortCode: 102030,
+          amount: 50,
+          date: yesterday,
+        },
+      ]
+
+      const result = transferFunds(
+        100,
+        10203040,
+        102030,
+        currentOriginTransactions,
+        false,
+        false,
+        50607080,
+        405060,
+        [],
+        false,
+        false,
+        now
+      )
+
+      expect(result.isSuccess).toBeFalsy()
+      expect(result.reason).toBe('Origin account has insufficient funds to complete transfer.')
+    })
+
+    it('does not transfer funds when the origin account is closed', () => {
+      const result = transferFunds(
+        100,
+        10203040,
+        102030,
+        [],
+        true,
+        false,
+        50607080,
+        405060,
+        [],
+        false,
+        false,
+        new Date()
+      )
+
+      expect(result.isSuccess).toBeFalsy()
+      expect(result.reason).toBe('Origin account is closed.')
+    })
+
+    it('does not transfer funds when the destination account is closed', () => {
+      const result = transferFunds(
+        100,
+        10203040,
+        102030,
+        [],
+        false,
+        false,
+        50607080,
+        405060,
+        [],
+        true,
+        false,
+        new Date()
+      )
+
+      expect(result.isSuccess).toBeFalsy()
+      expect(result.reason).toBe('Destination account is closed.')
+    })
+
+    it('does not transfer funds when origin account is frozen', () => {
+      const result = transferFunds(
+        100,
+        10203040,
+        102030,
+        [],
+        false,
+        true,
+        50607080,
+        405060,
+        [],
+        false,
+        false,
+        new Date()
+      )
+
+      expect(result.isSuccess).toBeFalsy()
+      expect(result.reason).toBe('Origin account is currently frozen.')
+    })
+
+    it('does not transfer funds when destination account is frozen', () => {
+      const result = transferFunds(
+        100,
+        10203040,
+        102030,
+        [],
+        false,
+        false,
+        50607080,
+        405060,
+        [],
+        false,
+        true,
+        new Date()
+      )
+
+      expect(result.isSuccess).toBeFalsy()
+      expect(result.reason).toBe('Destination account is currently frozen.')
     })
   })
 })
